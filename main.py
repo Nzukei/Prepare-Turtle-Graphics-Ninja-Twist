6bb192d208c317c1522ab956c22d37812e4422fa
N図形 <53482136+Nzukei@users.noreply.github.com>
2022-07-12 23:55:34 +0900

    Prepare-Turtle-Graphics-Ninja-Twist

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ ninja.speed(10)
 # 1: 가장 느림, 3: 느림, 6: 보통, 10: 빠름, 0: 가장 빠름
 
 for i in range(180):
-    ninja.forward(100)  # 100 픽셀 수 만큼 이동
-    ninja.right(30)     # 오른쪽 30°로 회전
-    ninja.forward(20)   # 20 픽셀 수 만큼 이동
-    ninja.left(60)      # 왼쪽 60°로 회전
-    ninja.forward(50)   # 50 픽셀 수 만큼 이동
-    ninja.right(30)     # 오른쪽 30°로 회전
+    ninja.forward(100)        # 100 픽셀 수 만큼 이동
+    ninja.right(30)           # 오른쪽 30°로 회전
+    ninja.forward(20)         # 20 픽셀 수 만큼 이동
+    ninja.left(60)            # 왼쪽 60°로 회전
+    ninja.forward(50)         # 50 픽셀 수 만큼 이동
+    ninja.right(30)           # 오른쪽 30°로 회전
 
     ninja.penup()             # 펜을 올려서 커서의 이동 흔적이 표시x
     ninja.setposition(0, 0)   # 커서를 (0, 0) 좌표로 이동
